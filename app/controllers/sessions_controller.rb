e740bc26abf18a0a6fdb0bedc6fe591d204600fa
Slawomir Cieslak <scieslak@scieslak.com>
2016-11-09 20:06:48 +0000

    Add create action to Sessions controller, add flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,22 @@
 class SessionsController < ApplicationController
   def new
+    # display log in form
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    user ||= User.find_by_email(params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "I have been logged in!"
+      redirect_to user
+    else
+      flash.now[:error] = "Invalid username or email/password combination!"
+      render 'new'
+
+    end
+
+
   end
 end
