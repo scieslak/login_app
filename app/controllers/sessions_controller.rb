a1654a65418e2e6948df41ec0df1767e0924c320
Slawomir Cieslak <scieslak@scieslak.com>
2016-11-09 21:15:09 +0000

    Add log out functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,14 +9,20 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = "I have been succesfully logged in!"
+      flash[:success] = "I have been succesfully logged in."
       redirect_to user
     else
       flash.now[:error] = "Invalid username or email/password combination!"
       render 'new'
-
     end
+  end
 
+
+  def destroy
+    session[:user_id] = nil
+    reset_session
+    flash[:success] = "You have been succesfully logged out."
+    redirect_to root_path
 
   end
 end
