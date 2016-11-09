4decaf32a6be6df192d856df011520144887c9d2
Slawomir Cieslak <scieslak@scieslak.com>
2016-11-09 18:18:14 +0000

    Generate Sessions controller update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+
   root "users#index"
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :users do
     member do
       get :delete
